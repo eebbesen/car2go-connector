0f5b2de34d9057fef58232ba0f70418c819833af
Eric Ebbesen <eebbesen.git@gmail.com>
2017-03-23 07:41:06 -0500

    Adding remaining endpoints

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
 # Car2Go Anypoint Connector
+Mule Anypoint connector for the Car2Go public API.
+* https://github.com/car2go/openAPI/wiki
+* https://www.mulesoft.com/platform/cloud-connectors
+
+## Car2Go API Oauth consumer key required
+Email openapi@car2go.com with your request for a key.  See https://github.com/car2go/openAPI/wiki for details.
+
+Provide the key to your program by sending the system property to the JVM thusly:
+
+`-DCAR2GO_CONSUMER_KEY=yoursecretkey`
 
 ## Supported operations
 Public functions from https://github.com/car2go/openAPI are exposed.  Except for `getLocations` all operations require a location parameter.
 
-All operations also require a Car2Go OAuth consumer token which you can request from openapi@car2go.com.
-
 ## Tests
-There's only one test per endpoint because I only care if we're getting the data in JSON format. These functional tests hit the Car2Go API for realz so you need a Car2Go API OAuth token for them to pass (see https://github.com/car2go/openAPI/wiki).  Feel free to mock that behavior out.
+There's only one test per endpoint because I only care if we're getting the data in JSON format. These functional tests hit the Car2Go API for realz so you need a Car2Go API OAuth token for them to pass (see https://github.com/car2go/openAPI/wiki).
 
-When running the tests with Maven (or any target that causes tests to run, e.g. `package`)you'll need a Car2Go OAuth consumer key assigned to `CAR2GO_CONSUMER_KEY`.
+You'll need a Car2Go OAuth consumer key assigned to `CAR2GO_CONSUMER_KEY`.
 
 `mvn test -DCAR2GO_CONSUMER_KEY=yoursecretkey`
-
-## Local usage
-This connector relies on a client named Car2Gosling which is not yet available in a public Maven repository (but which you can get from https://github.com/eebbesen/car2gosling).
-
-To run tests or package locally you'll need to execute the following.  It creates a local repo on your system that points to the lib directory in this project.
-
-    mvn deploy:deploy-file -DgroupId=com.humegatech -DartifactId=car2gosling -Dversion=1.1.2-SNAPSHOT -Durl=file:./lib/ -DrepositoryId=local-maven-repo -DupdateReleaseInfo=true -Dfile=./lib/car2gosling-1.1.2-SNAPSHOT.jar
--- src/test/java/com/humegatech/mule/modules/car2go/Car2GoConnectorTest.java
+++ src/test/java/com/humegatech/mule/modules/car2go/Car2GoConnectorTest.java
@@ -8,11 +8,28 @@ import java.util.List;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.humegatech.mule.modules.car2go.config.ConnectorConfig;
 
 public class Car2GoConnectorTest {
+    private static final String LOC = "Hamburg";
+
+    private Car2GoConnector connector;
+
+    @Before
+    public void setUp() {
+        final ConnectorConfig config = new ConnectorConfig(System.getProperty("CAR2GO_CONSUMER_KEY"));
+        connector = new Car2GoConnector();
+        connector.setConfig(config);
+    }
+
+    @After
+    public void tearDown() {
+        connector = null;
+    }
 
     @Test
     public void testConvertJSONArray() throws JSONException {
@@ -24,15 +41,48 @@ public class Car2GoConnectorTest {
     }
 
     @Test
-    public void getLocations() throws JSONException {
-        final ConnectorConfig config = new ConnectorConfig(System.getProperty("CAR2GO_CONSUMER_KEY"));
-        final Car2GoConnector connector = new Car2GoConnector();
-        connector.setConfig(config);
+    public void getGasStations() throws JSONException {
+        final String payload = connector.getGasStations(LOC);
 
+        final JSONArray gasStations = new JSONArray(payload);
+        final JSONObject gasStation = new JSONObject(gasStations.get(0).toString());
+        assertNotNull("No name for gas station -- may not have been properly built", gasStation.get("name"));
+    }
+
+    @Test
+    public void getLocations() throws JSONException {
         final String payload = connector.getLocations();
 
         final JSONArray locations = new JSONArray(payload);
         final JSONObject location = new JSONObject(locations.get(0).toString());
-        assertNotNull("No locationName for object -- may not have been properly built", location.get("locationName"));
+        assertNotNull("No locationName for location -- may not have been properly built", location.get("locationName"));
     }
+
+    @Test
+    public void getOperationAreas() throws JSONException {
+        final String payload = connector.getOperationAreas(LOC);
+
+        final JSONArray operationAreas = new JSONArray(payload);
+        final JSONObject operationArea = new JSONObject(operationAreas.get(0).toString());
+        assertNotNull("No zoneType for operation area -- may not have been properly built", operationArea.get("zoneType"));
+    }
+
+    @Test
+    public void getParkingSpots() throws JSONException {
+        final String payload = connector.getParkingSpots(LOC);
+
+        final JSONArray parkingSpots = new JSONArray(payload);
+        final JSONObject parkingSpot = new JSONObject(parkingSpots.get(0).toString());
+        assertNotNull("No chargingPole for parking spot -- may not have been properly built", parkingSpot.get("chargingPole"));
+    }
+
+    @Test
+    public void getVehicles() throws JSONException {
+        final String payload = connector.getVehicles(LOC);
+
+        final JSONArray vehicles = new JSONArray(payload);
+        final JSONObject vehicle = new JSONObject(vehicles.get(0).toString());
+        assertNotNull("No exterior for object -- may not have been properly built", vehicle.get("exterior"));
+    }
+
 }
